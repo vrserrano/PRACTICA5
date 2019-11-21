f114bfb6cad750151f4b163e1251d688d8e7ffac
Veronica Rodriguez <veronicarodriguezserrano@gmail.com>
2019-11-21 01:08:26 +0100

    formulario creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
 import { FormularioEditComponent } from './components/formulario-edit/formulario-edit.component';
 import { FormularioAddComponent } from './components/formulario-add/formulario-add.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,14 @@ import { FormularioAddComponent } from './components/formulario-add/formulario-a
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/components/formulario-add/formulario-add.component.ts
+++ src/app/components/formulario-add/formulario-add.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, NgZone, ErrorHandler } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+
+export interface Subject {
+  personName: string;
+}
 
 @Component({
   selector: 'app-formulario-add',
@@ -6,10 +13,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-add.component.css']
 })
 export class FormularioAddComponent implements OnInit {
-
-  constructor() { }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  @ViewChild('chipList', { static: true }) chipList;
+  @ViewChild('resetStudentForm', { static: true }) myNgForm;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  personForm: FormGroup;
+  subjectArray: Subject[] = [];
 
   ngOnInit() {
+    this.submitForm();
+  }
+  constructor(
+    public fb: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+
+  ) { }
+
+  /* Reactive book form */
+  submitForm() {
+    this.personForm = this.fb.group({
+      person_name: ['', [Validators.required]],
+      person_surname: ['', [Validators.required]],
+      person_age: ['', [Validators.required]],
+      person_id: ['', [Validators.required]],
+      person_birthday: ['', [Validators.required]],
+      person_color: ['', [Validators.required]],
+      person_sexo: ['', [Validators.required]]
+
+    })
+
+  }
+  public handleError = (controlName: string, errorName: string) => {
+    return this.personForm.controls[controlName].hasError(errorName);
   }
 
+
 }
+
+
