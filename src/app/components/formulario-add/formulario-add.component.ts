e8d146553777ad30e89d0dc1505a9341b7c187f4
Veronica Rodriguez <veronicarodriguezserrano@gmail.com>
2019-11-22 15:59:37 +0100

    Rango edad

--- src/app/components/formulario-add/formulario-add.component.ts
+++ src/app/components/formulario-add/formulario-add.component.ts
@@ -45,7 +45,23 @@ export class FormularioAddComponent implements OnInit {
       sexo_persona: ['', [Validators.required]]
 
     })
-
+    function validarRango(elemento){
+      var numero = parseInt(elemento.value,10);
+      //Validamos que se haya ingresado solo numeros
+      if(isNaN(numero)){
+        alert('Ingrese solo números.');
+        elemento.focus();
+        elemento.select();
+        return false;
+      }
+      //Validamos que se cumpla el rango
+      if(numero<0 || numero>125){
+        alert('Solo se permite el siguiente rango: 0 - 125');
+        elemento.focus();
+        return false;
+      }
+      return true;
+    }
   }
   public handleError = (controlName: string, errorName: string) => {
     return this.personaFormulario.controls[controlName].hasError(errorName);
